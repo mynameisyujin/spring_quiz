3100d9863ad1b46932894fe1c0cea6eb673ab80b
YUJIN <youjln5773@naver.com>
2023-09-20 20:49:52 +0900

    초기 환경 세팅

---
+++ src/main/java/com/quiz/lesson01/Lesson01Quiz03Controller.java
@@ -0,0 +1,13 @@
+package com.quiz.lesson01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Lesson01Quiz03Controller {
+	@RequestMapping("/lesson01/quiz03/1")
+	public String quiz03() {
+		// @ResponseBody 어노테이션이 없을 때 String을 리턴하면 ViewResolver에 의해 리턴된 String 경로의 jsp 화면을 찾고 태그가 Response Body에 담긴다.
+		return "lesson01/quiz03";
+	}
+}
